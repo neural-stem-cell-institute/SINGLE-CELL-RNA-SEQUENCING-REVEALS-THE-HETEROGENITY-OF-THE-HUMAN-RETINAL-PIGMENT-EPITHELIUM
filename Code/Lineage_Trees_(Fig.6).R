d3dbc5c048904eddce0560ef3cd2475861920db8
ncboles <50967614+ncboles@users.noreply.github.com>
2020-10-21 22:43:38 -0400

    Update Lineage_Trees_(Fig.6).R

--- Code/Lineage_Trees_(Fig.6).R
+++ Code/Lineage_Trees_(Fig.6).R
@@ -49,4 +49,26 @@ p<-p + theme_bw() + theme(axis.line = element_blank(),
 
 p
 
+#sessionInfo()
+#R version 4.0.2 (2020-06-22)
+#Platform: x86_64-w64-mingw32/x64 (64-bit)
+#Running under: Windows >= 8 x64 (build 9200)
 
+#Matrix products: default
+
+#locale:
+#[1] LC_COLLATE=English_United States.1252  LC_CTYPE=English_United States.1252    LC_MONETARY=English_United States.1252
+#[4] LC_NUMERIC=C                           LC_TIME=English_United States.1252    
+
+#attached base packages:
+#[1] stats     graphics  grDevices utils     datasets  methods   base     
+
+#other attached packages:
+#[1] ggplot2_3.3.2 foreach_1.5.0
+
+#loaded via a namespace (and not attached):
+# [1] codetools_0.2-16 withr_2.2.0      dplyr_1.0.1      crayon_1.3.4     grid_4.0.2       R6_2.4.1        
+# [7] lifecycle_0.2.0  gtable_0.3.0     magrittr_1.5     scales_1.1.1     pillar_1.4.6     rlang_0.4.7     
+#[13] rstudioapi_0.11  generics_0.0.2   vctrs_0.3.2      ellipsis_0.3.1   iterators_1.0.12 tools_4.0.2     
+#[19] glue_1.4.1       purrr_0.3.4      munsell_0.5.0    yaml_2.2.1       compiler_4.0.2   pkgconfig_2.0.3 
+#[25] colorspace_1.4-1 tidyselect_1.1.0 tibble_3.0.3  
